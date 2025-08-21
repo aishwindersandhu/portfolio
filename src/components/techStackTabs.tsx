9c0e250a93ed5487946473fe3eb7195f4a9f9023
aishwinder <aishwinder.sandhu@gmail.com>
2025-08-21 23:15:29 +0100

    Added tabs

--- src/components/techStackTabs.tsx
+++ src/components/techStackTabs.tsx
@@ -18,26 +18,26 @@ export const StackTabs = () => {
   const [activeTab, setActiveTab] = useState('Technologies');
   return (
     <div className="w-full max-w-md mx-auto p-4">
-      {/* Tabs navigation */}
-       <div className="flex gap-2 mb-4 justify-center">
+      {/* Tabs navigation as square rounded buttons */}
+      <div className="flex gap-2 mb-4 justify-center">
         {categories.map((tab) => (
           <button
             key={tab.name}
             onClick={() => setActiveTab(tab.name)}
-            className={`px-4 py-1 text-sm font-medium rounded-full transition-colors
-              ${
-                activeTab === tab.name
-                  ? "bg-[#B8AE88] text-white"
-                  : "bg-gray-200 text-gray-700 hover:bg-[#B8AE88] hover:text-white"
+            className={`px-4 py-1
+               border-[#B8AE88] 
+              text-sm rounded-md transition-colors
+              ${activeTab === tab.name
+                ? "bg-[#B8AE88] text-black"
+                : "bg-black text-[#B8AE88] hover:bg-[#B8AE88] hover:text-black border  border-[#B8AE88]"
               }`}
           >
             {tab.name}
           </button>
         ))}
       </div>
-
       {/* Tab content */}
-      {/* <div className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow">
+      {/* <div className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow text-center">
         {categories.find((tab) => tab.name === activeTab)?.content}
       </div> */}
     </div>
