e8342bcaa2850c62d08acd97c09401c0c6f9fde3
aishwinder <aishwinder.sandhu@gmail.com>
2025-08-22 14:11:06 +0100

    Changes to techstack content

---
+++ src/components/TabComponent.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export const TabComponent = ({ iconList }) => {
+  const renderToolkit = () => {
+    let iconsArr = [];
+    iconList.map((icon) => {
+      const iconName = icon.name.toLowerCase();
+      const iconDiv = <div className="relative group flex flex-col 
+text-center p-3 border w-20 h-20 
+border-white dark:border-white/10 rounded-xl 
+bg-white/30 dark:bg-white/10 shadow-2xl shadow-indigo-950/5"
+        style={{ width: '60px', height: '60px', justifyContent: 'center', alignItems: 'center' }}>
+        <a href={icon.link}
+          target="_blank"
+          rel="noopener noreferrer">
+          <img src={`/icons/${iconName}.svg`} alt={icon.name} className="w-10 h-10" />
+          <span className="absolute -bottom-8 left-1/2 -translate-x-1/2 
+                   bg-black text-white text-xs rounded px-2 py-1 opacity-0 
+                   group-hover:opacity-100 transition dark:bg-white dark:text-black z-50">
+            {icon.name}
+          </span>
+        </a>
+      </div>;
+      iconsArr.push(iconDiv);
+    })
+    return iconsArr;
+  }
+  return (
+    <div className="flex flex-row flex-wrap gap-2" style={{ justifyContent: 'center' }}>
+      {renderToolkit()}
+    </div>
+  )
+}
--- src/components/techStackTabs.tsx
+++ src/components/techStackTabs.tsx
@@ -1,35 +1,113 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+import { TabComponent } from './TabComponent';
 
 export const StackTabs = () => {
   const categories = [
     {
       name: "Technologies",
-      key: '#tech'
+      key: '#tech',
+      content: [{
+        name: 'JS',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+      },
+      {
+        name: 'HTML5',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'
+      }, {
+        name: 'CSS3',
+        link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
+      },
+      {
+        name: 'AngularJS',
+        link: 'https://angularjs.org/'
+      },
+      {
+        name: 'ReactJS',
+        link: 'https://react.dev/'
+      },
+      {
+        name: 'TypeScript',
+        link: 'https://www.typescriptlang.org/'
+      },
+      {
+        name: 'Redux',
+        link: 'https://redux.js.org/'
+      },
+      {
+        name: 'Webpack',
+        link: 'https://webpack.js.org/'
+      },
+      {
+        name: 'TailwindCSS',
+        link: 'https://tailwindcss.com/'
+      },
+      {
+        name: 'BootStrap4',
+        link: 'https://getbootstrap.com/'
+      },
+      {
+        name: 'Jest',
+        link: 'https://jestjs.io/'
+      },
+      {
+        name: 'Python',
+        link: 'https://www.python.org/'
+      }]
     },
     {
       name: "Business & Design",
-      key: '#business-design'
+      key: '#business-design',
+      content: [{
+        name: 'Jira',
+        link: 'https://www.atlassian.com/software/jira'
+      },
+      {
+        name: 'Figma',
+        link: 'https://www.figma.com/files/team/1303553213571263885/recents-and-sharing?fuid=1078645079340892952'
+      }]
     },
     {
       name: "Utilities",
-      key: '#utlities'
+      key: '#utlities',
+      content: [{
+        name: 'Git',
+        link: 'https://git-scm.com/'
+      },
+      {
+        name: 'Jenkins',
+        link: 'https://www.jenkins.io/'
+      },
+      {
+        name: 'Postman',
+        link: 'https://www.postman.com/'
+      },
+      {
+        name: 'Netlify',
+        link: 'https://www.netlify.com/'
+      }]
     }
   ];
   const [activeTab, setActiveTab] = useState('Technologies');
+  const { lightMode } = useContext(ThemeContext);
+  const styles = {
+    inactiveStyle: lightMode ? "bg-#[#B8AE88] text-black hover:bg-[#B8AE88] hover:text-black border  border-black" :
+      "bg-black text-[#B8AE88] hover:bg-[#B8AE88] hover:text-black border  border-[#B8AE88]"
+  }
   return (
-    <div className="w-full max-w-md mx-auto p-4">
+    <div className="w-full max-w-md mx-auto">
       {/* Tabs navigation as square rounded buttons */}
       <div className="flex gap-2 mb-4 justify-center">
         {categories.map((tab) => (
           <button
-            key={tab.name}
+            key={tab.key}
             onClick={() => setActiveTab(tab.name)}
             className={`px-4 py-1
                border-[#B8AE88] 
               text-sm rounded-md transition-colors
               ${activeTab === tab.name
                 ? "bg-[#B8AE88] text-black"
-                : "bg-black text-[#B8AE88] hover:bg-[#B8AE88] hover:text-black border  border-[#B8AE88]"
+                : styles.inactiveStyle
               }`}
           >
             {tab.name}
@@ -37,9 +115,16 @@ export const StackTabs = () => {
         ))}
       </div>
       {/* Tab content */}
-      {/* <div className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow text-center">
-        {categories.find((tab) => tab.name === activeTab)?.content}
-      </div> */}
+      <div className="px-3 rounded-lg text-center">
+        {
+          categories.map((tab)=>{
+            if(tab.name === activeTab){
+              return <TabComponent iconList ={tab.content}></TabComponent>
+            }
+          })
+        }
+      </div>
+
     </div>
   );
 }
