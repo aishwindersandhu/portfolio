d150607809669be4387a6eb7cf5f42c55ea513de
aishwinder <aishwinder.sandhu@gmail.com>
2025-08-26 17:18:35 +0100

    Changes for Footer and icons

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -12,10 +12,10 @@ export const Experience = () => {
     <React.Fragment>
       <section className={styles.experienceSectionClass} >
         <div className="w-full h-3/4">
-          <div className="mb-5 about-me-header latest-project-title">
+          <div className=" about-me-header latest-project-title">
             <span>EXPERIENCE</span>
             <span className={styles.launchArrow}>
-              <a href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://drive.usercontent.google.com/download?id=1enUL9tsOVM8e-8hBEuKQUnirUsoXOT5y&authuser=0&acrobatPromotionSource=GoogleDriveNativeView"
+              <a href="https://drive.google.com/file/d/1enUL9tsOVM8e-8hBEuKQUnirUsoXOT5y/view?raw=1"
                 target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="37-Arrow Up">
                   <path d="M30 18v7a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5h6V0H7a7 7 0 0 0-7 7v18a7 7 0 0 0 7 7h18a7 7 0 0 0 7-7v-7z" /><path d="M6.29 24.29 7.7 25.7 28 5.41V14h2V3a1 1 0 0 0-1-1H17v2h9.59z" /></g>
@@ -31,28 +31,41 @@ export const Experience = () => {
             <div className="mb-5 about-me-header experience-sub-heading">
             <div className="experience-data-heading">PROFESSIONAL EXPERIENCE</div>
             <div className="experience-data"> 
-              <a  href="https://www.wipro.com/" target="_blank"><b>Wipro Technologies, Consltant to HSBC - London (2020-2022) </b></a>
+              <a  href="https://www.wipro.com/"
+              className="experience-data-list-heading"
+               target="_blank"><b>Wipro Technologies, Consltant to HSBC - London (2020-2022) </b></a>
               <ul className="experience-data-list">
                 <li>Developed a Financial Dashboard for the employees of HSBC, for tracking their data ,clients data and deals.</li>
                 {/* Tabs for experience */}
               </ul>
             </div>
             <div className="experience-data">
-              <a  href="https://www.tripeasy.com/" target="_blank"><b>Internet Travel Solutions (2019-2020)</b></a>
+              <a  href="https://www.tripeasy.com/"
+             className="experience-data-list-heading"
+               target="_blank"><b>Internet Travel Solutions (2019-2020)</b></a>
               <ul className="experience-data-list">
                 <li>Led the migration of already existing product from AngularJS to ReactJS.</li>
                 {/* Tabs for experience */}
               </ul>
             </div>
             <div className="experience-data">
-              <a  href="https://www.questglobal.com/" target="_blank"><b>Mobiliya Technologies (2017-2019)</b></a>
+              <a  href="https://www.questglobal.com/" 
+               className="experience-data-list-heading"
+              target="_blank"><b>Mobiliya Technologies (2017-2019)</b></a>
                <ul className="experience-data-list">
                 <li>Consumed & constributed to HP's internal library - Veneer Native.</li>
                 <li>Awarded #3, highest code contributor in development.</li>
                 {/* Tabs for experience */}
               </ul>
               </div>
-            <div className="experience-data"><a  href="https://www.questglobal.com/" target="_blank"><b>Freelance Software Developer (2022-2024)</b></a></div>
+            <div className="experience-data">
+              <a  href="https://new-branch--condescending-montalcini-ddb8d9.netlify.app/"  className="experience-data-list-heading" target="_blank"><b>Freelance Software Developer (2022-2024)</b></a>
+                <ul className="experience-data-list">
+                <li>Implemented a Pets Website.</li>
+                <li>Implemented payments/subscription with Python and Chargebee.</li>
+                <li>Tailored UI for a progressive-web application(client-specific).</li>
+                {/* Tabs for experience */}
+              </ul></div>
           </div>
 
         </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,43 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
-import '../styles/footer.css'
+import '../styles/footer.css';
 
 export const Footer = () => {
   const { lightMode } = useContext(ThemeContext);
   const styles = {
-    footerSection: `${lightMode ? `bg-[#CEC4A7]` : `bg-black`} h-full about-section pt-2 p-5 w-full`
+    footerSection: `${lightMode ? `bg-[#CEC4A7]` : `bg-black`} h-full about-section pt-2 p-5 w-full`,
+    linkedinIcon: lightMode ? 'linkedinLight' : 'linkedinDark',
+    gmailIcon: lightMode ? 'gmailLight' : 'gmailDark',
+    githubIcon: lightMode ? 'githubLight' : 'githubDark'
   }
   return (
     <React.Fragment>
       <div className={styles.footerSection}>
         <div className="flex flex-row">
+          <div className="w-2/5 footerNote">
+            <div>Loosely designed in <a href="https://www.figma.com/design/jqBJpvykIWkLrIWbITWCmx/Aishwinder-Portfolio-Dark?node-id=0-1&p=f&t=YUlEc73l9fWzA1jA-0" target='_blank' className="footerTech">Figma </a>
+              and developed in <a href="https://code.visualstudio.com/" className="footerTech" target="blank">Visual Studio Code</a> by yours truly. Built with <a href="https://react.dev/" className="footerTech" target="_blank">React.js</a>,
+              <a href="https://www.typescriptlang.org/" className="footerTech" target="_blank">Typescript</a> and <a href="https://tailwindcss.com/" className="footerTech" target="_blank">Tailwind CSS</a>, deployed with <a href="https://www.netlify.com/" target="_blank" className="footerTech">Netlify</a>. </div>
+          </div>
           <div className="w-3/5">
-            <nav style={{ display: 'flex', justifyContent: 'left' }}>
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
               <a href="https://www.linkedin.com/in/aishwinder-sandhu-3b5002102/"
                 target="_blank"
-                className="header-nav" style={{ margin: '1rem 2rem 1rem 2rem' }} >LINKEDIN</a>
-              <a href="mailto:aishwinder.sandhu@gmail.com" className="header-nav" style={{ margin: '1rem 2rem 1rem 2rem' }}>GMAIL</a>
+                className="header-nav" style={{ margin: '1rem' }} >
+                <img src={`/icons/${styles.linkedinIcon}.svg`} />
+              </a>
               <a href="https://github.com/aishwindersandhu/"
-                target="_blank" className="header-nav" style={{ margin: '1rem 2rem 1rem 2rem' }}>GITHUB</a>
+                target="_blank" className="header-nav" style={{ margin: '1rem ' }}>
+                <img src={`/icons/${styles.githubIcon}.svg`} />
+              </a>
+              <a href="mailto:aishwinder.sandhu@gmail.com" className="header-nav" style={{ marginTop: '10px' }}>
+                <img src={`/icons/${styles.gmailIcon}.svg`} />
+              </a>
+
               <a href="#contact" className="header-nav"></a>
             </nav>
           </div>
-          <div className="w-2/5 footerNote">
-            <div>Loosely designed in <a href="https://www.figma.com/design/jqBJpvykIWkLrIWbITWCmx/Aishwinder-Portfolio-Dark?node-id=0-1&p=f&t=YUlEc73l9fWzA1jA-0" target='_blank' className="footerTech">Figma </a> 
-             and developed in <a href="https://code.visualstudio.com/" className="footerTech" target="blank">Visual Studio Code</a> by yours truly. Built with <a href="https://react.dev/" className="footerTech" target="_blank">React.js</a>,
-            <a href="https://www.typescriptlang.org/" className="footerTech" target="_blank">Typescript</a> and <a href="https://tailwindcss.com/" className="footerTech" target="_blank">Tailwind CSS</a>, deployed with <a href="https://www.netlify.com/" target="_blank" className="footerTech">Netlify</a>. </div>
-          </div>
-
         </div>
-
-
       </div>
     </React.Fragment>
   )
